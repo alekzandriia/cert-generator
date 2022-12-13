575fd79426c536b3e7f57a928f0eb50580989d04
Alekzandriia <alekzoreillyz@me.com>
2022-12-12 21:40:30 -0500

    connect form value to certificate text

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ const title = document.getElementById('title')
 const profile = document.getElementById('profile')
 const signature = document.getElementById('signature')
 const jobTitle = document.getElementById('job-title')
+const getAward = document.getElementById('get-award')
+const page = document.getElementById('page')
+const cert = document.getElementById('cert')
 
 profile.addEventListener('submit', (e) => {
   e.preventDefault
-  signature.textContent = title.value
-  
 })
+
+name.addEventListener('input', () => {
+  signature.textContent = name.value
+})
+
+title.addEventListener('input', () => {
+  jobTitle.textContent = title.value
+})
+
+getAward.addEventListener('click', () => {
+  page.classList.toggle("hidden")
+  cert.classList.toggle("hidden")
+})
+
